e1ba908c2a7752f608042f7656ef8534dc509249
Mahesh <Mahesh@MAHESH-LENOVO-LAPTOP>
2024-05-12 17:04:47 +0530

    Edited the file

--- SeleniumTraining/src/test/java/selenium/ASampleFile.java
+++ SeleniumTraining/src/test/java/selenium/ASampleFile.java
@@ -5,5 +5,6 @@ public class ASampleFile {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Mahesh Jeje");
+		System.out.println("Dhruv Jeje");
 	}
 }
