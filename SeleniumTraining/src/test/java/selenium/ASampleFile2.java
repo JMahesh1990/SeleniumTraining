84cea92a735b36a1e3ff9e0247ec9aeee89729e3
Mahesh <Mahesh@MAHESH-LENOVO-LAPTOP>
2024-05-13 12:28:48 +0530

    Pushing edited and new changes to repository.

--- SeleniumTraining/src/test/java/selenium/ASampleFile2.java
+++
@@ -1,8 +0,0 @@
-package selenium;
-
-public class ASampleFile2 {
-
-	public static void main(String[] args) {
-		System.out.println("Created a new java files");
-	}
-}
--- SeleniumTraining/src/test/java/selenium/Alert.java
+++ SeleniumTraining/src/test/java/selenium/Alert.java
@@ -2,6 +2,7 @@ package selenium;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
