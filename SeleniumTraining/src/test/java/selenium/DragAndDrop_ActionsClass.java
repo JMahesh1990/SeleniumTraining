14d13a89184861d29c6f4e4789cf8c7053df1ff6
Mahesh <Mahesh@MAHESH-LENOVO-LAPTOP>
2024-05-24 19:21:19 +0530

    Pushing the new changes...

--- SeleniumTraining/src/test/java/selenium/DragAndDrop_ActionsClass.java
+++ SeleniumTraining/src/test/java/selenium/DragAndDrop_ActionsClass.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
--- SeleniumTraining/src/test/java/selenium/MoveToElement2_ActionsClass.java
+++ SeleniumTraining/src/test/java/selenium/MoveToElement2_ActionsClass.java
@@ -1,5 +1,7 @@
 package selenium;
 
+import java.time.Duration;
+
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
@@ -11,7 +13,10 @@ public class MoveToElement2_ActionsClass {
 		System.setProperty("webdriver.http.factory", "jdk-http-client");
 		
 		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+		
 		Actions act = new Actions(driver);
+		JavascriptExecutor js = (JavascriptExecutor) driver;
 		
 		driver.manage().window().maximize();	
 		driver.get("https://demo.automationtesting.in/Register.html");
@@ -25,6 +30,20 @@ public class MoveToElement2_ActionsClass {
 			.moveToElement(drafanddrop).click()
 			.moveToElement(staticc).click().build().perform();
 		
+		Thread.sleep(2000);
+		
+		js.executeScript("window.scrollBy(0,400)");
+		
+		WebElement angular = driver.findElement(By.xpath("//*[@id='angular']"));
+		WebElement mongo = driver.findElement(By.xpath("//*[@id='mongo']"));
+		WebElement node = driver.findElement(By.xpath("//*[@id='node']"));
+		WebElement droparea = driver.findElement(By.xpath("//*[@id='droparea']"));
+		
+		act.dragAndDrop(angular, droparea).build().perform();
+		Thread.sleep(2000);
+		act.dragAndDrop(mongo, droparea).build().perform();
+		Thread.sleep(2000);
+		act.dragAndDrop(node, droparea).build().perform();
 		
 		Thread.sleep(5000);
 		driver.close();
